bfb201e33a49fdf4bc9c440aa69a4b0d0e255f33
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-04-19 17:13:15 +0200

    automatically add all controller modules to requirejs-rails modules

--- lib/requirejs/controllers/rails/engine.rb
+++ lib/requirejs/controllers/rails/engine.rb
@@ -2,7 +2,18 @@ module Requirejs
   module Controllers
     module Rails
       class Engine < ::Rails::Engine
-        # here be automagic
+        config.before_initialize do |app|
+          js_path = app.paths['app/assets'].to_a.select {|p| p =~ /javascripts/}.first
+
+          # find all controller modules and strip the filext
+          modules = Dir[File.join(js_path, 'controllers/**/*')].select do |fn|
+            File.file?(fn)
+          end.map do |fn|
+            {'name' => fn.sub(js_path + '/', '').sub(/\.js.*$/, '')}
+          end
+
+          config.requirejs.user_config['modules'] = modules
+        end
       end
     end
   end
