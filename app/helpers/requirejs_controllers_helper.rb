0fd064f656ed6e80c92c06d00059c4d1a1d5694f
Dandy <dandy.fenz@liquidm.com>
2017-04-21 16:39:58 +0200

    rails4 compatibility

--- app/helpers/requirejs_controllers_helper.rb
+++ app/helpers/requirejs_controllers_helper.rb
@@ -3,10 +3,8 @@ module RequirejsControllersHelper
     html = requirejs_include_tag
     html.concat <<-HTML.html_safe
     <script>
-    define('main', function() {
-      require(['controllers/#{controller.controller_path}'], function(controller) {
-        window.controller = new controller('#{controller.action_name}')
-      })
+    require(['controllers/#{controller.controller_path}'], function(controller) {
+      window.controller = new controller('#{controller.action_name}')
     })
     </script>
     HTML
